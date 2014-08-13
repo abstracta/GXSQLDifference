1fe76a48730330bbce32a22de62429fc0f29c461
andreig09 <andreig09@gmail.com>
2014-08-13 15:42:10 -0300

    First version of the app

---
+++ Backup/GXSQLDifference/SQLData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXSQLDifference
+{
+    class SQLData
+    {
+        public double TotalTime { get; set; }
+        public double Count { get; set; }
+        public double Average { get; set; }
+        public double WorstTime { get; set; }
+        public double BestTime { get; set; }
+
+
+        internal string GetDifData(SQLData sQLData)
+        {
+            string result = "";
+            if (sQLData!=null)
+            {
+                //totaltime(dif);count(dif);post average;pre averga;best time (post);worstTime(post)";
+                result = (TotalTime - sQLData.TotalTime) + ";" + (Count - sQLData.Count) + ";" + Average + ";" + sQLData.Average + ";" + BestTime + ";" + WorstTime;
+
+            }else
+	        {
+                result = TotalTime+";"+Count+";"+Average+";"+0+";"+BestTime+";"+WorstTime;
+	        }
+            return result;
+        }
+    }
+}
